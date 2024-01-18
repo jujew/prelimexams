71f8e4ec7d3fbaebc90bf43d383e9fb7ca1b650c
jujew <144343249+jujew@users.noreply.github.com>
2024-01-18 19:42:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,24 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> 
- <p>   10 Things That Require Zero Talent  </p>
-<p>
-<ul> <li> 1. Being On Time </li>
-<li>2. Making An Effort </li>
-<li>3. Being High Energy </li>
-<li>4. Having A Positive Attitude </li>
-<li>5. Being Passionate </li>
-<li>6. Using Good Body Language </li>
-<li>7. Being Coachable </li>
-<li>8. Doing A Little Extra </li>
-<li>9. Being Prepared </li>
-<li>10. Having A Strong Work Ethic </ul> </p>
+      <p><b>Warup brah! I am Jewel Ocampo rAwr!! </b></p>
+        <p><b>10 Things That Require Zero Talent</b></p>
+        <ul>
+        <li>1. Being on Time</li>
+        <li>2. Making An Effort</li>
+        <li>3. Being High Energy</li>
+        <li>4. Having A Positive Attitude</li>
+        <li>5. Being Passionate</li>
+        <li>6. Using Good Body Language</li>
+        <li>7. Being Coachable</li>
+        <li>8. Doing A Little Extra</li>
+        <li>9. Being Prepared</li>
+        <li>10. Having A Strong Work Ethic</li>
+        </ul>
+        <p><b>3 Things to expect to this subject</b></p>
+        <li>1. Easy</li>
+        <li>2. Interesting</li>
+        <li>3. Unforgettable</li>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
