65bf3ea9e40bf2824ea52e0ce0312be25492591f
jujew <144343249+jujew@users.noreply.github.com>
2024-02-15 12:12:30 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,14 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{site title}</title>
+        <title></title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p><b>10 Things That Require Zero Talent</b></p>
-        <ul>
-        <li>1. Being on Time</li>
-        <li>2. Making An Effort</li>
+
+    <p>10 Things That Require Zero Talent </p>
+<ul>
+        <li>1. Being On Time </li>
+        <li>2. Making An Effort </li>
         <li>3. Being High Energy</li>
         <li>4. Having A Positive Attitude</li>
         <li>5. Being Passionate</li>
@@ -21,7 +22,7 @@ export default function Home() {
         <li>8. Doing A Little Extra</li>
         <li>9. Being Prepared</li>
         <li>10. Having A Strong Work Ethic</li>
-        </ul>
+  </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
